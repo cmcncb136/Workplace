9f2323b422ead2f06608165b848d2eea305eadb5
SDE00 <songdongeun00@gmail.com>
2024-05-21 21:05:40 +0900

    #design: list_View xml 구현
    아래 xml 구현
    listview_list_products.xml
    listview_list_prodct_detail.xml

---
+++ app/src/main/java/com/heybuddy/safespace/ProductListActivity.kt
@@ -0,0 +1,82 @@
+package com.heybuddy.safespace
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.heybuddy.safespace.databinding.ProductBuyBinding
+import com.heybuddy.safespace.product.ProductInfo
+
+class ProductListActivity: AppCompatActivity() {
+
+    private lateinit var bind: ProductBuyBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bind = ProductBuyBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+        val adapter = ProductsListAdapter()
+
+        adapter.addItem(ProductInfo("Hello"))
+        adapter.addItem(ProductInfo("Hello"))
+        adapter.addItem(ProductInfo("Hello"))
+        adapter.addItem(ProductInfo("Hello"))
+
+        bind.productsBook.adapter = adapter
+        setListViewHeightBaseOnChildren(bind.productsBook)
+
+        bind.productsBook.adapter = adapter
+        setListViewHeightBaseOnChildren(bind.productsMovie)
+    }
+
+    fun setListViewHeightBaseOnChildren(list: ListView){
+        val adapter = list.adapter ?: return
+        var totalHeight = 0
+        for(i in 0 until adapter.count){
+            val item = adapter.getView(i, null, list)
+            item.measure(0, 0)
+            totalHeight += item.measuredHeight
+        }
+
+        val params = list.layoutParams
+        params.height = totalHeight + (list.dividerHeight * (list.count) - 1)
+        list.layoutParams = params
+        list.requestLayout()
+    }
+
+}
+
+
+class ProductsListAdapter: BaseAdapter(){
+    val products: ArrayList<ProductInfo> = ArrayList()
+
+    override fun getCount(): Int {
+        return  products.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return  products[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position as Long;
+    }
+
+    fun addItem(s: ProductInfo){
+        products.add(s)
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val context = parent!!.context
+        val s = products[position]
+
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.listview_list_products, parent, false)
+
+        return view
+    }
+}
